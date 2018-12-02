abef7ec5c2ae42184271daa0d4e8263148ffd804
vescoc <claudio.vesco@gmail.com>
2018-12-02 09:08:55 +0100

    scalafmt

--- src/main/scala/aoc/Day02.scala
+++ src/main/scala/aoc/Day02.scala
@@ -13,18 +13,21 @@ object Day02 {
     val solution1 = input
       .map { l =>
         val set = l
-          .groupBy { c => c }
-          .map { v => (v._1, v._2.size) }
+          .groupBy { c =>
+            c
+          }
+          .map { v =>
+            (v._1, v._2.size)
+          }
           .foldLeft(Set[Int]()) { (acc, value) =>
             acc + value._2
           }
 
-        def exists(n: Int) = {
+        def exists(n: Int) =
           if (set.contains(n))
             1
           else
             0
-        }
 
         (exists(2), exists(3))
       }
@@ -33,27 +36,34 @@ object Day02 {
           a <- acc
         } yield { (a._1 + value._1, a._2 + value._2) } // pigrizia :P
       }
-      .map { p => p._1 * p._2 }
+      .map { p =>
+        p._1 * p._2
+      }
 
     println(s"solution 1: ${solution1.get}")
 
     @tailrec
     def solve2(index: Int = 0): String = {
-      def helper(index: Int = index, input: List[String] = input) = {
+      def helper(index: Int = index, input: List[String] = input) =
         input
           .map { l =>
             new StringBuilder(l).deleteCharAt(index).toString
           }
-          .groupBy { l => l }
-          .map { p => (p._1, p._2.size) }
-          .filter { p => p._2 > 1 }
-      }
+          .groupBy { l =>
+            l
+          }
+          .map { p =>
+            (p._1, p._2.size)
+          }
+          .filter { p =>
+            p._2 > 1
+          }
 
       val map = helper()
       val size = map.size
       size match {
-        case 0 => solve2(index + 1)
-        case 1 => map.toIterator.next._1
+        case 0    => solve2(index + 1)
+        case 1    => map.toIterator.next._1
         case _ @m => sys.error(s"invalid solution: $m")
       }
     }
