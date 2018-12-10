ee6d329d8a43a113bc113bddcb2ba29447a89e00
vescoc <claudio.vesco@gmail.com>
2018-12-10 18:18:59 +0100

    autotuning

--- scalajs/src/main/scala/aoc/Day10.scala
+++ scalajs/src/main/scala/aoc/Day10.scala
@@ -1,8 +1,9 @@
 package aoc
 
+import scala.scalajs.js.timers._
 import org.scalajs.dom
-import dom.document
-import dom.html
+import org.scalajs.dom.{document, ext, html}
+import ext.KeyCode
 
 object Day10 {
   val SmallDelta = 1
@@ -37,23 +38,76 @@ object Day10 {
     document.body.appendChild(canvas)
 
     var currentTime = 0
+    var autotuneMode = false
+    var autotuneDirection = 0
+
+    var timeoutHandle: SetTimeoutHandle = null
+    var minArea: Long = Long.MaxValue
+    var currentArea: Long = Long.MaxValue
+
+    def startAutotune() =
+      if (!autotuneMode) {
+        autotuneMode = true
+        autotuneDirection = SmallDelta
+
+        currentTime += autotuneDirection
+
+        render()
+      }
+
+    def stopAutotune() =
+      if (autotuneMode) {
+        clearTimeout(timeoutHandle)
+
+        autotuneMode = false
+        timeoutHandle = null
+
+        render()
+      }
+
+    def changeAutotuneDirection() =
+      if (autotuneDirection == 0 || autotuneDirection < 0)
+        +1
+      else
+        -1
+
+    def sameAutotuneDirection() =
+      if (autotuneDirection == 0)
+        +1
+      else
+        autotuneDirection
+
+    def disableAutotuneMode() = {
+      if (autotuneMode) {
+        clearTimeout(timeoutHandle)
+      }
+
+      autotuneMode = false
+      timeoutHandle = null
+    }
+
+    def addCurrentTime(delta: Int) = {
+      disableAutotuneMode()
+      currentTime += delta
+      render()
+    }
 
     dom.window.addEventListener(
       "keydown",
       (e: dom.KeyboardEvent) => {
         e.keyCode match {
-          case 39 => // right arrow
-            currentTime += SmallDelta
-            render()
-          case 37 => // left arrow
-            currentTime -= SmallDelta
-            render()
-          case 38 => // up arrow
-            currentTime += BigDelta
-            render()
-          case 40 => // down arrow
-            currentTime -= BigDelta
-            render()
+          case KeyCode.Right =>
+            addCurrentTime(SmallDelta)
+          case KeyCode.Left =>
+            addCurrentTime(-SmallDelta)
+          case KeyCode.Up =>
+            addCurrentTime(BigDelta)
+          case KeyCode.Down =>
+            addCurrentTime(-BigDelta)
+          case KeyCode.A =>
+            startAutotune()
+          case KeyCode.P =>
+            stopAutotune()
           case _ =>
           // nothing
         }
@@ -61,8 +115,6 @@ object Day10 {
     )
 
     def render(): Unit = {
-      println(s"render $currentTime")
-
       ctx.clearRect(0.0, 0.0, canvas.width.toDouble, canvas.height.toDouble)
 
       val currentPoints = input
@@ -76,13 +128,38 @@ object Day10 {
           )
         }
 
+      val area = (maxPoint.x - minPoint.x).toLong * (maxPoint.y - minPoint.y).toLong
+
+      autotuneDirection =
+        if (area == minArea)
+          0
+        else if (area > currentArea)
+          changeAutotuneDirection()
+        else
+          sameAutotuneDirection()
+
+      minArea = Math.min(minArea, area)
+      currentArea = area
+
       currentTimeParagraph.innerHTML =
-        s"Current time: $currentTime -- [$minPoint - $maxPoint] [${maxPoint.x - minPoint.x}, ${maxPoint.y - minPoint.y}]"
+        s"""Current time: $currentTime -- [$minPoint - $maxPoint] [${maxPoint.x - minPoint.x}, ${maxPoint.y - minPoint.y}] Autotune: ${if (autotuneMode)
+          "On"
+        else "Off"}"""
 
       currentPoints
         .foreach { star =>
           ctx.fillRect(star.p.x.toDouble, star.p.y.toDouble, 1, 1)
         }
+
+      if (autotuneMode)
+        if (autotuneDirection == 0)
+          stopAutotune()
+        else
+          timeoutHandle = setTimeout(1) {
+            currentTime += autotuneDirection
+
+            render()
+          }
     }
 
     render()
