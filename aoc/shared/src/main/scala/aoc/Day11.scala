22f8fd980361dd51e23bd35d666b4899d1039277
vescoc <claudio.vesco@gmail.com>
2018-12-12 19:19:08 +0100

    day 11 with summed up left table algo

--- aoc/shared/src/main/scala/aoc/Day11.scala
+++ aoc/shared/src/main/scala/aoc/Day11.scala
@@ -12,7 +12,7 @@ object Day11 {
     val OptMode = Try { args(1).toInt } getOrElse 0
     val GridSerialNumber = Try { args(2).toInt } getOrElse 5153
 
-    val powerGrid = Array.tabulate(GridWidth, GridWidth) { (x, y) =>
+    def powerGridXY(x: Int, y: Int): Int = {
       val X = x + 1
       val Y = y + 1
 
@@ -25,151 +25,208 @@ object Day11 {
 
     type Info = ((Int, Int, Int), Int)
 
-    def solveBrutal(range: Range): Info = {
-      (
-        (
-          for {
-            d <- range
-          } yield {
-            val inner: Seq[Info] = (
-              for {
-                x <- 0 until GridWidth
-                y <- 0 until GridHeight
-              } yield {
-                val cell: ((Int, Int, Int), Option[Int]) =
-                  (
-                    (x + 1, y + 1, d) ->
-                      (
-                        (
-                          for {
-                            dx <- x until x + d
-                            dy <- y until y + d
-                          } yield {
-                            Try { Some(powerGrid(dx)(dy)) } getOrElse None
-                          }
-                        ).reduce { (oa: Option[Int], ob: Option[Int]) =>
-                          for {
-                            a <- oa
-                            b <- ob
-                          } yield (a + b)
-                        }
-                      )
-                  )
-                cell
-              }
-            ).collect { case (k @ _, Some(v @ _)) => (k, v) }
+    val sumUpLeftSolvers = {
+      val sumUpLeft = Array.ofDim[Int](GridWidth, GridHeight)
 
-            inner
-          }
-        )
-      )
-        .flatten
-        .maxBy { _._2 }
-    }
+      def value(x: Int)(y: Int): Int =
+        if (x >= 0 && y >= 0)
+          sumUpLeft(x)(y)
+        else
+          0
 
-    def solveOpt(range: Range): Info = {
-      @tailrec
-      def calc(current: Info, x: Int, y: Int, dimensions: Iterator[Int]): Info =
-        if (y == GridHeight)
-          current
-        else if (dimensions.hasNext) {
-          val dim = dimensions.next
-          if (x + dim > GridWidth || y + dim > GridHeight)
-            calc(current, x, y, dimensions)
-          else {
-            @tailrec
-            def sum(s: Int, dx: Int, dy: Int): Int =
-              if (dy == dim)
-                s
-              else {
-                val ns = s + powerGrid(x + dx)(y + dy)
-                val (ndx, ndy) = {
-                  if (dx + 1 >= dim)
-                    (0, dy + 1)
-                  else
-                    (dx + 1, dy)
-                }
-                sum(ns, ndx, ndy)
-              }
+      for {
+        y <- 0 until GridHeight
+        x <- 0 until GridWidth
+      } {
+        sumUpLeft(x)(y) = powerGridXY(x, y) - value(x - 1)(y - 1) + value(x - 1)(y) + value(x)(y - 1)
+      }
 
-            val candidate = sum(0, 0, 0)
-            if (candidate > current._2)
-              calc(((x + 1, y + 1, dim), candidate), x, y, dimensions)
-            else
+      def solve(range: Range): Info = {
+        @tailrec
+        def calc(current: Info, x: Int, y: Int, dimensions: Iterator[Int]): Info =
+          if (y == GridHeight)
+            current
+          else if (dimensions.hasNext) {
+            val dim = dimensions.next
+            if (x + dim >= GridWidth || y + dim >= GridHeight)
               calc(current, x, y, dimensions)
-          }
-        } else {
-          val (nx, ny) = {
-            if (x + 1 >= GridHeight) {
-              val r = (0, y + 1)
-              r
-            } else
-              (x + 1, y)
-          }
-          calc(current, nx, ny, range.iterator)
-        }
-
-      calc(((0, 0, 0), Int.MinValue), 0, 0, range.iterator)
-    }
-
-    def solveOpt2(range: Range): Info = {
-      val max = powerGrid
-        .map { _.max }
-        .max
-
-      val iterator = range.iterator
-
-      @tailrec
-      def calc(current: Info, x: Int, y: Int, dim: Int): Info =
-        if (x + dim > GridWidth || y + dim > GridHeight) {
-          if (x + 1 >= GridWidth) {
-            if (y + 1 >= GridHeight) {
-              if (iterator.hasNext) {
-                val ndim = iterator.next
-
-                calc(current, 0, 0, ndim)
-              } else {
-                current
-              }
-            } else {
-              calc(current, 0, y + 1, dim)
+            else {
+              val candidate = value(x + dim - 1)(y + dim - 1) + value(x - 1)(y - 1) - value(x + dim - 1)(y - 1) - value(x - 1)(y + dim - 1)
+              if (candidate > current._2)
+                calc(((x + 1, y + 1, dim), candidate), x, y, dimensions)
+              else
+                calc(current, x, y, dimensions)
             }
           } else {
-            calc(current, x + 1, y, dim)
+            val (nx, ny) = {
+              if (x + 1 >= GridHeight) {
+                val r = (0, y + 1)
+                r
+              } else
+                  (x + 1, y)
+            }
+            calc(current, nx, ny, range.iterator)
           }
-        } else {
-          @tailrec
-          def sum(maxRemainder: Int, s: Int, dx: Int, dy: Int): Option[Int] =
-            if (dy == dim) {
-              Some(s)
-            } else {
-              val ns = s + powerGrid(x + dx)(y + dy)
-              if (ns + maxRemainder < current._2)
-                None
-              else {
-                val (ndx, ndy) = {
-                  if (dx + 1 >= dim)
-                    (0, dy + 1)
-                  else
-                    (dx + 1, dy)
+
+        calc(((0, 0, 0), Int.MinValue), 0, 0, range.iterator)
+      }
+
+      List(solve _)
+    }
+
+    val powerGridSolvers = {
+      val powerGrid = Array.tabulate(GridWidth, GridHeight) { powerGridXY }
+
+      def solveBrutal(range: Range): Info = {
+        (
+          (
+            for {
+              d <- range
+            } yield {
+              val inner: Seq[Info] = (
+                for {
+                  x <- 0 until GridWidth
+                  y <- 0 until GridHeight
+                } yield {
+                  val cell: ((Int, Int, Int), Option[Int]) =
+                    (
+                      (x + 1, y + 1, d) ->
+                        (
+                          (
+                            for {
+                              dx <- x until x + d
+                              dy <- y until y + d
+                            } yield {
+                              Try { Some(powerGrid(dx)(dy)) } getOrElse None
+                            }
+                          ).reduce { (oa: Option[Int], ob: Option[Int]) =>
+                            for {
+                              a <- oa
+                              b <- ob
+                            } yield (a + b)
+                          }
+                        )
+                    )
+                  cell
+                }
+              ).collect { case (k @ _, Some(v @ _)) => (k, v) }
+
+              inner
+            }
+          )
+        )
+          .flatten
+          .maxBy { _._2 }
+      }
+
+      def solveOpt(range: Range): Info = {
+        @tailrec
+        def calc(current: Info, x: Int, y: Int, dimensions: Iterator[Int]): Info =
+          if (y == GridHeight)
+            current
+          else if (dimensions.hasNext) {
+            val dim = dimensions.next
+            if (x + dim > GridWidth || y + dim > GridHeight)
+              calc(current, x, y, dimensions)
+            else {
+              @tailrec
+              def sum(s: Int, dx: Int, dy: Int): Int =
+                if (dy == dim)
+                  s
+                else {
+                  val ns = s + powerGrid(x + dx)(y + dy)
+                  val (ndx, ndy) = {
+                    if (dx + 1 >= dim)
+                      (0, dy + 1)
+                    else
+                      (dx + 1, dy)
+                  }
+                  sum(ns, ndx, ndy)
                 }
 
-                sum(maxRemainder - max, ns, ndx, ndy)
-              }
+              val candidate = sum(0, 0, 0)
+              if (candidate > current._2)
+                calc(((x + 1, y + 1, dim), candidate), x, y, dimensions)
+              else
+                calc(current, x, y, dimensions)
             }
-
-          sum(max * dim * dim, 0, 0, 0) match {
-            case Some(v) if v > current._2 =>
-              calc(((x + 1, y + 1, dim), v), x + 1, y, dim)
-            case _ =>
-              calc(current, x + 1, y, dim)
+          } else {
+            val (nx, ny) = {
+              if (x + 1 >= GridHeight) {
+                val r = (0, y + 1)
+                r
+              } else
+                  (x + 1, y)
+            }
+            calc(current, nx, ny, range.iterator)
           }
-        }
 
-      calc(((0, 0, 0), Int.MinValue), 0, 0, iterator.next)
+        calc(((0, 0, 0), Int.MinValue), 0, 0, range.iterator)
+      }
+
+      def solveOpt2(range: Range): Info = {
+        val max = powerGrid
+          .map { _.max }
+          .max
+
+        val iterator = range.iterator
+
+        @tailrec
+        def calc(current: Info, x: Int, y: Int, dim: Int): Info =
+          if (x + dim > GridWidth || y + dim > GridHeight) {
+            if (x + 1 >= GridWidth) {
+              if (y + 1 >= GridHeight) {
+                if (iterator.hasNext) {
+                  val ndim = iterator.next
+
+                  calc(current, 0, 0, ndim)
+                } else {
+                  current
+                }
+              } else {
+                calc(current, 0, y + 1, dim)
+              }
+            } else {
+              calc(current, x + 1, y, dim)
+            }
+          } else {
+            @tailrec
+            def sum(maxRemainder: Int, s: Int, dx: Int, dy: Int): Option[Int] =
+              if (dy == dim) {
+                Some(s)
+              } else {
+                val ns = s + powerGrid(x + dx)(y + dy)
+                if (ns + maxRemainder < current._2)
+                  None
+                else {
+                  val (ndx, ndy) = {
+                    if (dx + 1 >= dim)
+                      (0, dy + 1)
+                    else
+                      (dx + 1, dy)
+                  }
+
+                  sum(maxRemainder - max, ns, ndx, ndy)
+                }
+              }
+
+            sum(max * dim * dim, 0, 0, 0) match {
+              case Some(v) if v > current._2 =>
+                calc(((x + 1, y + 1, dim), v), x + 1, y, dim)
+              case _ =>
+                calc(current, x + 1, y, dim)
+            }
+          }
+
+        calc(((0, 0, 0), Int.MinValue), 0, 0, iterator.next)
+      }
+
+      List(solveOpt2 _, solveOpt _, solveBrutal _)
     }
+
+    val solvers = sumUpLeftSolvers ++ powerGridSolvers
     
-    val solve = List(solveOpt2 _, solveOpt _, solveBrutal _)(OptMode)
+    val solve = solvers(OptMode)
 
     val solution1 = solve(3 to 3)
 
