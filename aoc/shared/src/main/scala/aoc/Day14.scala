7f34bc0fbf548cdd3a0332b025f5907eb6ae0aba
vescoc <claudio.vesco@gmail.com>
2018-12-14 15:44:19 +0100

    day 14 / 2 not working...

--- aoc/shared/src/main/scala/aoc/Day14.scala
+++ aoc/shared/src/main/scala/aoc/Day14.scala
@@ -6,6 +6,9 @@ object Day14 {
 
     lazy val recipes: Stream[Int] = {
       def loop(firstIndex: Int, secondIndex: Int, size: Int): Stream[Int] = {
+        if (size % 10000 == 0)
+          println(s"size: $size")
+
         val first = recipes(firstIndex)
         val second = recipes(secondIndex)
 
@@ -33,11 +36,31 @@ object Day14 {
     recipes
   }
 
+  def takeAfter(recipes: Stream[Int], take: Int, after: Int): String =
+    recipes.drop(after).take(take).mkString
+
+  def findFirstRecurrence(recipes: Stream[Int], input: String, from: Int = 0): Int =
+    recipes
+      .sliding(input.size)
+      .map { _.mkString }
+      .indexOf(input, from)
+
   def main(args: Array[String]): Unit = {
     val input = 170641
 
     val recipes = recipesMaker(37)
 
-    println(s"""solution 1: ${recipes.drop(input).take(10).mkString}""")
+    //println(s"solution 1: ${takeAfter(recipes, 10, input)}")
+
+    //println(s"solution 2: ${findFirstRecurrence(recipes, input.toString)}")
+    //println(s"""solution 2: ${findFirstRecurrence(recipes, "7064")}""")
+    val i = findFirstRecurrence(recipes, "706")
+    println(i -> takeAfter(recipes, 10, i - 4))
+
+    val j = findFirstRecurrence(recipes, "706", i + 2)
+    println(j -> takeAfter(recipes, 10, j - 4))
+
+    val k = findFirstRecurrence(recipes, "706", j + 2)
+    println(k -> takeAfter(recipes, 10, k - 4))
   }
 }
--- aoc/shared/src/test/scala/aoc/Day14Spec.scala
+++ aoc/shared/src/test/scala/aoc/Day14Spec.scala
@@ -5,21 +5,29 @@ import org.scalatest.{MustMatchers, WordSpec}
 import aoc.Day14._
 
 class Day14Spec extends WordSpec with MustMatchers {
-  "recipesMaker" must {
-    "equals 5158916779 with initial 37" in {
-      recipesMaker(37).drop(9).take(10).mkString mustBe ("5158916779")
-    }
+  val recipes = recipesMaker(37)
 
-    "equals 0124515891 with initial 37 after 5 recipes" in {
-      recipesMaker(37).drop(5).take(10).mkString mustBe ("0124515891")
-    }
+  "part 1 recipes" must {
+    def testTakeAfter(input: String, after: Int) =
+      s"equals $input after $after" in {
+        takeAfter(recipes, 10, after) mustBe (input)
+      }
 
-    "equals 9251071085 with initial 37 after 18 recipes" in {
-      recipesMaker(37).drop(18).take(10).mkString mustBe ("9251071085")
-    }
+    testTakeAfter("5158916779", 9)
+    testTakeAfter("0124515891", 5)
+    testTakeAfter("9251071085", 18)
+    testTakeAfter("5941429882", 2018)
+  }
 
-    "equals 5941429882 with initial 37 after 2018 recipes" in {
-      recipesMaker(37).drop(2018).take(10).mkString mustBe ("5941429882")
-    }
+  "part 2" must {
+    def testFindFirstRecurrence(input: String, index: Int) =
+      s"$input appears after $index recipes" in {
+        findFirstRecurrence(recipes, input) mustBe (index)
+      }
+
+    testFindFirstRecurrence("51589", 9)
+    testFindFirstRecurrence("01245", 5)
+    testFindFirstRecurrence("92510", 18)
+    testFindFirstRecurrence("59414", 2018)
   }
 }
