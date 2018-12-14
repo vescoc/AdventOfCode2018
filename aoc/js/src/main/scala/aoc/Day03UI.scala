4c19410ff4b03d61cc9414bd02e9a060ad20255b
vescoc <claudio.vesco@gmail.com>
2018-12-14 21:27:41 +0100

    day 3 user interface

---
+++ aoc/js/src/main/scala/aoc/Day03UI.scala
@@ -0,0 +1,45 @@
+package aoc
+
+import org.scalajs.dom
+import org.scalajs.dom.{document, ext, html}
+
+import aoc.Day03._
+
+object Day03UI {
+  final val Mag = 0.95
+
+  def main(args: Array[String]): Unit = {
+    val (minX, minY, maxX, maxY) = input
+      .foldLeft((Int.MaxValue, Int.MaxValue, Int.MinValue, Int.MinValue)) { (acc, r) =>
+        (Math.min(acc._1, r.x), Math.min(acc._2, r.y), Math.max(acc._3, r.x + r.w), Math.max(acc._4, r.y + r.h))
+      }
+
+    val container = document.createElement("div").asInstanceOf[html.Div]
+    container.style = s"width: ${Math.min((Mag * dom.window.innerWidth).toInt, maxX)}px;height:${Math.min((Mag * dom.window.innerHeight).toInt, maxY)}px;overflow:auto;border:1px solid;"
+
+    val canvas = document.createElement("canvas").asInstanceOf[html.Canvas]
+    canvas.style = "display:block;"
+
+    container.appendChild(canvas)
+    document.body.appendChild(container)
+
+    canvas.width = maxX
+    canvas.height = maxY
+
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
+    implicit class RectangleColor(r: Rectangle) {
+      def color(): ext.Color =
+        ext.Color.all(1 + r.id % (ext.Color.all.size - 1))
+    }
+
+    def render(): Unit = {
+      input foreach { r =>
+        ctx.fillStyle = r.color.toHex
+        ctx.fillRect(r.x.toDouble, r.y.toDouble, (r.x + r.w).toDouble, (r.y + r.h).toDouble)
+      }
+    }
+
+    render()
+  }
+}
--- aoc/shared/src/main/scala/aoc/Day03.scala
+++ aoc/shared/src/main/scala/aoc/Day03.scala
@@ -1,8 +1,11 @@
 package aoc
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 object Day03 {
+  val Pure = true
+
   val re = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".r
 
   case class Rectangle(
@@ -13,57 +16,113 @@ object Day03 {
     h: Int
   )
 
-  def main(args: Array[String]): Unit = {
-    val input = Source
-      .fromResource("input-03.data")
-      .getLines()
-      .map { l =>
-        l match {
-          case re(id, x, y, w, h) => Rectangle(id.toInt, x.toInt, y.toInt, w.toInt, h.toInt)
-        }
+  val input = Source
+    .fromResource("input-03.data")
+    .getLines()
+    .map { l =>
+      l match {
+        case re(id, x, y, w, h) => Rectangle(id.toInt, x.toInt, y.toInt, w.toInt, h.toInt)
       }
-      .toList
+    }
+    .toList
 
-    val map = mutable.Map[(Int, Int), mutable.Set[Int]]()
-    val solution1 = input
-      .foldLeft(0) { (acc, r) =>
-        var a = acc
+  type Info = Map[(Int, Int), Set[Int]]
 
-        for (x <- r.x until (r.x + r.w)) {
-          for (y <- r.y until (r.y + r.h)) {
-            map.get((x, y)) match {
-              case None =>
-                map((x, y)) = mutable.Set(r.id)
-              case Some(set) =>
-                if (set.size == 1)
-                  a = a + 1
-                set += r.id
+  def main(args: Array[String]): Unit = {
+    if (Pure) {
+      val (solution1, map) = input
+        .foldLeft((0, Map.empty[(Int, Int), Set[Int]])) { (acc, r) =>
+          val (a, map) = acc
+
+          @tailrec
+          def helper(a: Int = a, map: Info = map, x: Int = r.x, y: Int = r.y): (Int, Info) = {
+            val (na, nmap) = {
+              map.get((x, y)) match {
+                case None =>
+                  (a, map + ((x, y) -> Set(r.id)))
+                case Some(set) =>
+                  (if (set.size == 1) a + 1 else a, map + ((x, y) -> (set + r.id)))
+              }
+            }
+
+            val (nx, ny) = {
+              if (x + 1 == r.x + r.w)
+                (r.x, y + 1)
+              else
+                (x + 1, y)
+            }
+
+            if (ny == r.y + r.h)
+              (na, nmap)
+            else
+              helper(na, nmap, nx, ny)
+          }
+
+          helper()
+        }
+
+      println(s"solution 1: $solution1")
+
+      val solution2 = {
+        val ids = input
+          .foldLeft(Set.empty[Int]) { (acc, r) =>
+            acc + r.id
+          }
+
+        val overlapIds = map
+          .foldLeft(Set.empty[Int]) { (acc, p) =>
+            if (p._2.size > 1)
+              acc ++ p._2
+            else
+              acc
+          }
+
+        ids -- overlapIds
+      }
+
+      println(s"solution 2: $solution2")
+    } else {
+      val map = mutable.Map[(Int, Int), mutable.Set[Int]]()
+      val solution1 = input
+        .foldLeft(0) { (acc, r) =>
+          var a = acc
+
+          for (x <- r.x until (r.x + r.w)) {
+            for (y <- r.y until (r.y + r.h)) {
+              map.get((x, y)) match {
+                case None =>
+                  map((x, y)) = mutable.Set(r.id)
+                case Some(set) =>
+                  if (set.size == 1)
+                    a = a + 1
+                  set += r.id
+              }
             }
           }
+
+          a
         }
 
-        a
+      println(s"solution 1: $solution1")
+
+      val solution2 = {
+        val ids = input
+          .foldLeft(Set.empty[Int]) { (acc, r) =>
+            acc + r.id
+          }
+
+        val overlapIds = map
+          .foldLeft(Set.empty[Int]) { (acc, p) =>
+            if (p._2.size > 1)
+              acc ++ p._2
+            else
+              acc
+          }
+
+        ids -- overlapIds
       }
 
-    println(s"solution 1: $solution1")
-
-    val solution2 = {
-      val ids = input
-        .foldLeft(Set.empty[Int]) { (acc, r) =>
-          acc + r.id
-        }
-
-      val overlapIds = map
-        .foldLeft(Set.empty[Int]) { (acc, p) =>
-          if (p._2.size > 1)
-            acc ++ p._2
-          else
-            acc
-        }
-
-      ids -- overlapIds
+      println(s"solution 2: $solution2")
     }
-
-    println(s"solution 2: $solution2")
   }
 }
